0bf4588fd450e434354fa73dea51d18c1deec929
Wolfred Montilla <wolfrednicolas@gmail.com>
2024-02-12 17:32:01 -0500

    add predeterminate route when not matching

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import WelcomeView from '@/views/WelcomeView.vue';
+import { useAuthStore, useAlertStore } from '@/stores';
 
 const routes = [
   {
     path:'/',
     name:'WelcomeView',
     component: WelcomeView
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: '/' 
   }
 ]
 
 export const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
+  linkActiveClass: 'active',
   routes
+});
+
+router.beforeEach(async (to) => {
+  // clear alert on route change
+  const alertStore = useAlertStore();
+  alertStore.clear();
 });
