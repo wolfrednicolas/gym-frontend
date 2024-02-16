4fd90d56063fdc375dd30fd33005ae317197cee6
Wolfred Montilla <wolfrednicolas@gmail.com>
2024-02-16 09:59:45 -0500

    redifine routes for admin, users, login and sign up

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import WelcomeView from '@/views/WelcomeView.vue';
-import { useAuthStore, useAlertStore } from '@/stores';
+import DashboardViewAdmin from '@/views/admin/dashboard/DashboardView.vue';
+import DashboardViewUser from '@/views/user/DashboardView.vue';
+import {  useAlertStore } from '@/stores';
 
 const routes = [
   {
     path:'/',
-    name:'WelcomeView',
-    component: WelcomeView
+    name:'login',
+    component: WelcomeView,
+    props:{ showLogin: true }
+  },
+  {
+    path:'/signup',
+    name:'signup',
+    component: WelcomeView,
+    props:{ showLogin: false }
+  },
+  {
+    path:'/users',
+    name:'users',
+    component: DashboardViewUser,
+  },
+  {
+    path:'/admin',
+    name:'admin',
+    component: DashboardViewAdmin,
   },
   {
     path: '/:pathMatch(.*)*',
@@ -24,4 +43,24 @@ router.beforeEach(async (to) => {
   // clear alert on route change
   const alertStore = useAlertStore();
   alertStore.clear();
+  console.log("donde debe ir: "+to.path)
+  
+
+  // // redirect to login page if not logged in and trying to access a restricted page 
+  // const publicPages = ['/account/login', '/account/register'];
+  // const authRequired = !publicPages.includes(to.path);
+
+  // const authStore = useAuthStore();
+  // authStore.returnUrl = to.fullPath
+
+  //return authStore.returnUrl;
+
+  // if(!authStore.user_id){
+  //   return "/login"
+  // }
+
+  // if (authRequired && !authStore.user) {
+  //     authStore.returnUrl = to.fullPath;
+  //     return '/account/login';
+  // }
 });
