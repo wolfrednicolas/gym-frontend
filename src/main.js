6e45b2e549588d478a8e0c9341d26f2b434b7ef0
Wolfred Montilla <wolfrednicolas@gmail.com>
2024-02-16 09:59:15 -0500

    add bootstrap to main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import { router } from './router'
 import { createPinia } from 'pinia';
 
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+
 
 const app = createApp(App);
 
